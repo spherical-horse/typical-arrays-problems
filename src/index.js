6e7d4205211333ca5a4481f228966a09c9e4a67a
Artem <tema.shevchuk@gmail.com>
2021-02-26 19:11:59 +0300

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let ans = Infinity;
+    array.forEach(item => {
+        if (item < ans) {
+            ans = item;
+        }
+    });
+    return ans;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let ans = -Infinity;
+    array.forEach(item => {
+        if (item > ans) {
+            ans = item;
+        }
+    });
+    return ans;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let acc = 0;
+    array.forEach(item => {
+        acc = acc + item;
+    });
+    return acc / array.length;
 }
